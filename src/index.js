6bfbbfc6cf36128e6c9b58670b218a4660b859f3
AndrewMik <andrew.mikitchuk@gmail.com>
2023-01-16 14:31:05 +0400

    feat: implement solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+  let result = "";
+
+  n = n < 0 ? n*-1 : n;
+
+  for (let i = 0; i < n.toString().length; i++){
+    result += n.toString()[n.toString().length-1-i];
+  }
+
+  return result;
 }
